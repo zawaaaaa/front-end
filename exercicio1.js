fea136e839d1b06fcc9daf5a2e8b2bba69540757
zawaaaaa <126490185+zawaaaaa@users.noreply.github.com>
2024-03-26 12:19:53 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,10 @@
+//vetor A com seis numeros inteiros
+const A=[1,0,5,-2,-5,7];
+//soma dos valores das posiçoes A[0], A[1] e a A[5]
+const soma= A[0] + A[1]+ A[5];
+console.log("soma dos valores das posiçoes A[0], A[1], A[5]:");
+//modificando o valor na posiçao 4 (indice 3) para 100
+A[3]= 100;
+//mostrando cada valor no vetor A, um em cada linha 
+console.log("\nValores no vetor A:");
+A.forEach(valor => console.log (valor));
