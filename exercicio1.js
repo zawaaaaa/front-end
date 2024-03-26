711ada133f460acd569a658cf490d42c485d5d1a
zawaaaaa <126490185+zawaaaaa@users.noreply.github.com>
2024-03-26 12:28:12 -0300

    exercicio1.js

--- exercicio1.js
+++ exercicio1.js
@@ -2,7 +2,7 @@
 const A=[1,0,5,-2,-5,7];
 //soma dos valores das posiçoes A[0], A[1] e a A[5]
 const soma= A[0] + A[1]+ A[5];
-console.log("soma dos valores das posiçoes A[0], A[1], A[5]:");
+console.log("soma dos valores das posiçoes A[0], A[1], A[5]:",soma);
 //modificando o valor na posiçao 4 (indice 3) para 100
 A[3]= 100;
 //mostrando cada valor no vetor A, um em cada linha 
