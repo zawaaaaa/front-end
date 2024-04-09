aad40d76c520d5c7d5604d15ba06629ad96dfbc1
zawaaaaa <126490185+zawaaaaa@users.noreply.github.com>
2024-04-09 12:37:02 -0300

    Add files via upload

---
+++ exercicio3.js
@@ -0,0 +1,7 @@
+const myURL = nem URL('https://example.org:81/foo');
+console.log(myURL.host);
+//prints example.org:81
+
+myURL.host = 'example.com:82';
+console.log(myURL.href);
+//prints https://example.com:82/foo
