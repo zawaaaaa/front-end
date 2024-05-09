a18a96a7604cd5811d9cb1274974ed5d41c8f9cb
zawaaaaa <126490185+zawaaaaa@users.noreply.github.com>
2024-05-09 12:37:26 -0300

    Add files via upload

---
+++ sort.js
@@ -0,0 +1,14 @@
+function selectionSort(array);{
+ for (i=0; i <array.lenght, i++){
+ let aux= array [i];
+   for(j=1; array.lengt;){
+    if (array [i]> array [j]){
+        array[i]=array[j]
+        array [i+1]=aux;
+      }   }        
+    }
+    return array;
+}  
+  let dado=[1,5,7,8];
+console.log(selection
+    Sort(dados)  );
